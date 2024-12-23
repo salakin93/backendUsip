295bba8ea4afbf55ca30e913933c80fe40e1945a
Alejandro Salazar <asalazarm@pilandina.com.bo>
2024-12-23 08:47:27 -0400

    first commit

---
+++ src/main/java/edu/usip/library/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package edu.usip.library.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}

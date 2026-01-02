fe2a5791fd1c53ab6a15c405f580dff0b0de7b28
Alejandro Salazar <asalazarm@pilandina.com.bo>
2026-01-01 23:55:45 -0400

    first commit

---
+++ pdfdocumentmanager/pdfdocumentmanager/src/main/java/edu/usip/pdfdocumentmanager/PdfdocumentmanagerApplication.java
@@ -0,0 +1,13 @@
+package edu.usip.pdfdocumentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfdocumentmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfdocumentmanagerApplication.class, args);
+	}
+
+}
---
+++ pdfdocumentmanager/pdfdocumentmanager/src/test/java/edu/usip/pdfdocumentmanager/PdfdocumentmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.usip.pdfdocumentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfdocumentmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
